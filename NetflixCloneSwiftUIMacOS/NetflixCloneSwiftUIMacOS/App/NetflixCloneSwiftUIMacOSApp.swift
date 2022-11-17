6550ef2c725c5267c93a274a3505463457687961
Barış Özgen <barisozgen@hotmail.com>
2022-11-17 12:47:36 +0300

    Resources and assets created

---
+++ NetflixCloneSwiftUIMacOS/NetflixCloneSwiftUIMacOS/App/NetflixCloneSwiftUIMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneSwiftUIMacOSApp.swift
+//  NetflixCloneSwiftUIMacOS
+//
+//  Created by Baris OZGEN on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneSwiftUIMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
